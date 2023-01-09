041f8c73d215d2da14ca22f1e3098378845dd9e7
Zhivko Shopov <103967363+JiksiJs@users.noreply.github.com>
2023-01-09 14:08:07 +0000

    Added functionality
    the texture now collects the list items when the button is clicked

--- DOM-Intro-Lab/02.Collect List Items/CollectListItems.js
+++ DOM-Intro-Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,5 @@
 function extractText() {
-    // TODO
+  let items = Array.from(document.querySelectorAll("li"));
+  let result = items.map((e) => e.textContent);
+  document.getElementById("result").textContent = result;
 }
