80e67b5ae3945150834d403213b8cba7b5fc39c2
Zhivko Shopov <103967363+JiksiJs@users.noreply.github.com>
2023-01-09 13:57:34 +0000

    Add files via upload
    Solution added.

--- DOM-Intro-Lab/01.Edit Element/EditElement.js
+++ DOM-Intro-Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-  // TODO
+function editElement(element, match, replacer) {
+  let text = element.textContent;
+  let result = text.split(match).join(replacer);
+  document.getElementById("e1").textContent = result;
 }
