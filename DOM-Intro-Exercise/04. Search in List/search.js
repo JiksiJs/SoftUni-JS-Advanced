d792064d85946705e27486c86599e48763cccad8
Zhivko Shopov <103967363+JiksiJs@users.noreply.github.com>
2023-02-20 12:43:12 +0000

    Add files via upload
    Solution added

--- DOM-Intro-Exercise/04. Search in List/search.js
+++ DOM-Intro-Exercise/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+  let towns = Array.from(document.querySelectorAll("li"));
+  let searchInput = document.getElementById("searchText").value;
+  let counter = 0;
+
+  for (let town of towns) {
+    town.style.fontWeight = "normal";
+    town.style.textDecoration = "none";
+
+    if (town.textContent.includes(searchInput)) {
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+      counter++;
+    }
+  }
+  document.getElementById("result").textContent = `${counter} matches found`;
 }
