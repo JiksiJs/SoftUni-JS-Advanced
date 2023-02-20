ef70d6146636bb51ba0200fdd226dd8d8f8484b3
Zhivko Shopov <103967363+JiksiJs@users.noreply.github.com>
2023-02-20 14:58:48 +0000

    Solution added
    When the button is pressed, the table row is removed if there is a match with the input

--- DOM-Events-Lab/02. Delete-From-Table/app.js
+++ DOM-Events-Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,14 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const input = document.querySelector("input[type=text]").value;
+  let rows = document.querySelectorAll("tbody tr");
+  let result = document.getElementById("result");
+
+  for (let row of rows) {
+    if (row.lastElementChild.textContent.includes(input)) {
+      row.remove();
+      result.textContent = "Deleted";
+    } else {
+      result.textContent = "Not found.";
+    }
+  }
 }
