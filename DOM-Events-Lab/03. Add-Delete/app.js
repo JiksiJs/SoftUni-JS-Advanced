4fd6cc062d1230b8e2d041e437590e1aba61040e
Zhivko Shopov <103967363+JiksiJs@users.noreply.github.com>
2023-02-20 15:10:15 +0000

    Delta button added
    When Delete button is clicked, removes the list element

--- DOM-Events-Lab/03. Add-Delete/app.js
+++ DOM-Events-Lab/03. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    //TODO...
+  const input = document.getElementById("newItemText").value;
+  const list = document.getElementById("items");
+  let newList = document.createElement("li");
+  newList.textContent = input;
+  list.appendChild(newList);
+
+  let deleteBtn = document.createElement("a");
+  deleteBtn.textContent = "[Delete]";
+  deleteBtn.href = "#";
+  newList.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", deleteList);
+
+  function deleteList() {
+    newList.remove();
+  }
 }
